4d3bd303efc4028349fe596b22fd04a1a2c2aeac
jsky119 <87289235+jsky119@users.noreply.github.com>
2021-10-04 18:44:14 +0900

    Recap redux basic

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+//reducer가 return하는 것은 app의 state가 됨
 const countModifier = (count = 0, action) => {
   console.log(count, action);
   if (action.type === "ADD") {
@@ -21,10 +22,12 @@ const onChange = () => {
   number.innerText = countStore.getState();
 };
 
+//store에서 변화를 감지하기 위해 subscribe를 사용
 countStore.subscribe(onChange);
 
+//reducer에게 action을 보낼 땐 dispatch를 사용
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: "ADD" }); //action은 object여야만 하며 type이 있어야 한다
 };
 add.addEventListener("click", handleAdd);
 
