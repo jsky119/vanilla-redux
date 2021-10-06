c1f20ac58a330097f967e3bd225eb6876163f163
jsky119 <87289235+jsky119@users.noreply.github.com>
2021-10-06 13:57:50 +0900

    Create single use ToDo

--- src/index.js
+++ src/index.js
@@ -1,47 +1,18 @@
-import { createStore } from "redux";
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
-
-number.innerText = 0;
-
-const ADD = "ADD";
-const MINUS = "MINUS";
-
-//reducer가 return하는 것은 app의 state가 됨
-const countModifier = (count = 0, action) => {
-  // console.log(count, action);
-  // if (action.type === "ADD") {
-  //   return count + 1;
-  // } else if (action.type === "MINUS") {
-  //   return count - 1;
-  // } else {
-  //   return count;
-  // }
-  switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
-    default:
-      return count;
-  }
+const createToDo = (toDo) => {
+  const li = document.createElement("li");
+  li.innerText = toDo;
+  ul.appendChild(li);
 };
 
-const countStore = createStore(countModifier);
-
-const onChange = () => {
-  number.innerText = countStore.getState();
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  createToDo(toDo);
 };
 
-//store에서 변화를 감지하기 위해 subscribe를 사용
-countStore.subscribe(onChange);
-
-//reducer에게 action을 보낼 땐 dispatch를 사용
-const handleAdd = () => {
-  countStore.dispatch({ type: ADD }); //action은 object여야만 하며 type이 있어야 한다
-};
-add.addEventListener("click", handleAdd);
-
-minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
+form.addEventListener("submit", onSubmit);
