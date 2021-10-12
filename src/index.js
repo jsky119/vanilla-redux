079ac281bc53e33038cc8282a0f39fa3e42248f2
jsky119 <87289235+jsky119@users.noreply.github.com>
2021-10-12 20:07:50 +0900

    Recap toDo code

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
 const addToDo = (text) => {
+  /*object를 reducer의 action으로 return하며 
+  return한 object는 dispatch를 위해 이용된다
+  */
   return {
     type: ADD_TODO,
     text,
@@ -25,9 +28,18 @@ const reducer = (state = [], action) => {
   console.log(action);
   switch (action.type) {
     case ADD_TODO:
-      return [{ text: action.text, id: Date.now() }, ...state];
+      /* toDos array를 mutate하지 않고 새로 만들어 사용한다
+    새로운 array를 만들고 이전에 있던 array의 내용에
+    addToDo에서 return한 object를 추가하여 적용한다
+    */
+      const newToDoObj = { text: action.text, id: Date.now() };
+      return [newToDoObj, ...state];
     case DELETE_TODO:
-      return state.filter((toDo) => toDo.id !== action.id);
+      /*마찬가지로 array에서 object를 삭제하지 않는다
+      filter를 이용하여 state에서 삭제할 object를 제외한 array를 새로 만든다
+    */
+      const cleaned = state.filter((toDo) => toDo.id !== action.id);
+      return cleaned;
     default:
       return state;
   }
@@ -53,7 +65,7 @@ const paintToDos = () => {
   });
 };
 
-store.subscribe(paintToDos);
+store.subscribe(paintToDos); //toDo의 변화에 맞게 list를 repainting
 
 const dispatchAddToDo = (text) => {
   store.dispatch(addToDo(text));
